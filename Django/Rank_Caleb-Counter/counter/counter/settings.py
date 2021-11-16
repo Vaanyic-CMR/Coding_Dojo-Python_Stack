4392a1c9a3374981bd0ca9be661d773bbf1c3d2a
Caleb M Rank <calebmrank@gmail.com>
2021-11-15 18:36:22 -0700

    commit 11/15/2021

--- Django/Rank_Caleb-Counter/counter/counter/settings.py
+++ Django/Rank_Caleb-Counter/counter/counter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g)m+=^!#crefge$p@g7=*xp1ht9wpmuo*579q5qbx9h_&(0%n8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Django/Rank_Caleb-Dojo_Survey/dojo_survey/dojo_survey/settings.py
+++ Django/Rank_Caleb-Dojo_Survey/dojo_survey/dojo_survey/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(*gwx9p=3bu07ic%!kc1@e=@g=cic@pg0mg6r8k7e&yrm6gdht'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
